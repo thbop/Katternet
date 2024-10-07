b5ee5a5658cd35a22c7401bab086811595ab9744
Thbop <75861321+thbop@users.noreply.github.com>
2024-10-06 21:36:22 -0400

    server -> client for client.c

--- client/client.c
+++ client/client.c
@@ -4,7 +4,7 @@
 int main() {
     // printf("Hello World!\n");
     TNET_Init();
-    SOCKET sock = TNET_socket(NULL, "5000");
+    SOCKET sock = TNET_socket("127.0.0.1", "5000");
 
     TNET_Free();
     return 0;
