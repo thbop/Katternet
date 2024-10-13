1898ebf3b709ae51a44bf5278997a4398cdc88a7
Thbop <75861321+thbop@users.noreply.github.com>
2024-10-13 18:25:04 -0400

    Fixed collisions... now it needs cleanup

--- client/client.c
+++ client/client.c
@@ -13,15 +13,13 @@ int main() {
     int a = 12;
     int b = 34;
 
-    HM hashmap = HM_init(10);
-    HM_set(hashmap, "age", &a);
-    HM_set(hashmap, "number", &b);
+    HM hashmap = HM_init(6);
+    HM_set(&hashmap, "number", &a);
+    HM_set(&hashmap, "water", &b);
 
-    printf(
-        "Age: %d, Number: %d\n",
-        *(int*)HM_get(hashmap, "age"),
-        *(int*)HM_get(hashmap, "number")
-    );
+    
+
+    printf("%d\n", *(int*)HM_get(&hashmap, "water"));
 
     HM_free(hashmap);
 
--- client/lib/hashmap.h
+++ client/lib/hashmap.h
@@ -4,18 +4,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define HM_MAX_KEY_LENGTH 16
+
 typedef struct HM_pair {
     size_t key;
+    // char *strkey;
+    char strkey[HM_MAX_KEY_LENGTH];
     void *item, *next;
 } HM_pair;
 
 typedef struct HM {
-    HM_pair *hashmap;
+    HM_pair **hashmap;
     size_t size;
 } HM;
 
 HM HM_init( size_t size ) {
-    return (HM){ (HM_pair*)malloc( sizeof(HM_pair)*size ), size };
+    return (HM){ (HM_pair**)calloc( size, sizeof(HM_pair*) ), size };
 }
 
 void HM_free( HM hashmap ) {
@@ -43,14 +47,52 @@ size_t HM_hash( const char* strkey ) {
     return key*key2;
 }
 
-void HM_set( HM hashmap, const char* strkey, void *item ) {
-    size_t key = HM_hash(strkey);
-    hashmap.hashmap[key % hashmap.size].item = item;
+// for ( lastpair = pair; pair->next != NULL; lastpair = pair->next );
+
+#define HM_str_to_hstr( src, dst ) \
+    dst = malloc(sizeof(src)); \
+    strcpy_s(dst, strlen(src), src);
+
+
+int HM_set( HM *hashmap, char *strkey, void *item ) {
+    if ( strlen(strkey) > HM_MAX_KEY_LENGTH ) {
+        printf("HASHMAP ERROR: Key \"%s\" length exceeds HM_MAX_KEY_LENGTH = %d!\n", strkey, HM_MAX_KEY_LENGTH);
+        return 1;
+    }
+    size_t key = HM_hash(strkey) % hashmap->size;
+    // printf("Create key: %d\n", key);
+    HM_pair *pair;
+    if ( hashmap->hashmap[key] == NULL ) {
+        pair = (HM_pair*)calloc(1, sizeof(HM_pair)); // Needs clean up
+        pair->item = item;
+        // HM_str_to_hstr(strkey, pair->strkey);
+        strcpy_s(pair->strkey, HM_MAX_KEY_LENGTH, strkey);
+        hashmap->hashmap[key] = pair;
+        // printf("New pair\n");
+    }
+    else {
+        pair = hashmap->hashmap[key];
+        HM_pair *newpair = (HM_pair*)calloc(1, sizeof(HM_pair)); // Needs clean up
+        // HM_str_to_hstr(strkey, newpair->strkey);
+        strcpy_s(newpair->strkey, HM_MAX_KEY_LENGTH, strkey);
+        newpair->item = item;
+        newpair->next = pair;
+        hashmap->hashmap[key] = newpair;
+        // printf("Collision\n");
+    }
+    return 0;
 }
 
-void *HM_get( HM hashmap, const char* strkey ) {
-    size_t key = HM_hash(strkey);
-    return hashmap.hashmap[key % hashmap.size].item;
+void *HM_get( HM *hashmap, const char* strkey ) {
+    size_t key = HM_hash(strkey) % hashmap->size;
+    HM_pair *pair = hashmap->hashmap[key];
+    
+    while ( pair ) {
+        // printf("pair->strkey: %s\n", pair->strkey);
+        if ( !strncmp(strkey, pair->strkey, HM_MAX_KEY_LENGTH) ) return pair->item;
+        pair = pair->next;
+    }
+    return NULL;
 }
 
 
